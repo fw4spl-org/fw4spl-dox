1ef076c2299f9c8f263916403cf1d0fefaf393f1
julien.waechter <julien.waechter@gmail.com>
2016-01-18 15:24:09 +0100

    Updated gh-page to display links for generated doxygen

---
+++ README.rst
@@ -0,0 +1,4 @@
+fw4spl-dox
+==========
+
+This repository contains the generated doxygen for fw4spl (https://github.com/fw4spl-org/fw4spl)
---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+
+$.getJSON("https://api.github.com/repos/fw4spl-org/fw4spl-dox/branches", function (branches) {
+    var cdn = "https://cdn.rawgit.com/fw4spl-org/fw4spl-dox/$BRANCH/index.html";
+    $('#content').empty();
+    for (var i = 0; i < branches.length; i++) {
+        var name = branches[i].name;
+        if(name.match("^fw4spl")){
+            var printName = name.toUpperCase().replace("_", "&nbsp;");
+            var url = cdn.replace("$BRANCH", name);
+            var branch = '<a class="btn btn-primary btn-lg" href="' + url + '" role="button">' + printName + '</a>';
+            $('#content').append(branch);
+        }
+    }
+});
+
